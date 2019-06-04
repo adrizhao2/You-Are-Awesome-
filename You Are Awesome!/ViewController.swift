af6c816677687036606cbb0276fd41ffd2999087
Adrianna Zhao <zhaocx@bc.edu>
2019-06-04 15:51:54 -0400

    2.7 Complete

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var messageLabel: UILabel!
-    var index = 0
     
     // Code below executes when the app's view first loads
     override func viewDidLoad() {
@@ -20,37 +19,19 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showMessagePressed(_ sender: UIButton) {
-        let messages = ["You Are Fantastic!!!",
-                        "You Are Great!",
-                        "You Are Amazing!",
-                        "When the Genius Bar needs help, they call you",
-                        "You Brighten My Day!",
-                        "You are da bomb!",
-                        "Hey, fabulous!",
-                        "You are tremendous!",
-                        "You've got the design skills of Jony Ive!",
-                        "I can't wait to download your app!"]
         
-        messageLabel.text = messages[index]
-        if index == messages.count - 1 {
-            index = 0
+        let message1 = "You Are Awesome!"
+        let message2 = "You Are Great!"
+        let message3 = "You Are Amazing!"
+        
+        if messageLabel.text == message1 {
+            messageLabel.text = message2
+        } else if messageLabel.text == message2 {
+            messageLabel.text = message3
         } else {
-            index += 1
+            messageLabel.text = message1
         }
-        
-//        let message1 = "You Are Fantastic!!!"
-//        let message2 = "You Are Great!"
-//        let message3 = "You Are Amazing!"
-//
-//        if messageLabel.text == message1 {
-//            messageLabel.text = message2
-//        } else if messageLabel.text == message2 {
-//            messageLabel.text = message3
-//        } else {
-//            messageLabel.text = message1
-//        }
-        
     }
-        
+    
 }
 
