a623d341cb0c8656dbf2bbfdda18351c5b945af5
Adrianna Zhao <zhaocx@bc.edu>
2019-06-05 14:21:19 -0400

    2.18 Complete

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -26,46 +26,15 @@ class ViewController: UIViewController {
         
     }
     
-    @IBAction func showMessagePressed(_ sender: UIButton) {
-        let messages = ["You Are Fantastic!!!",
-                        "You Are Great!",
-                        "You Are Amazing!",
-                        "When the Genius Bar needs help, they call you",
-                        "You Brighten My Day!",
-                        "You are da bomb!",
-                        "Hey, fabulous!",
-                        "You are tremendous!",
-                        "You've got the design skills of Jony Ive!",
-                        "I can't wait to download your app!"]
-        
-        var newIndex: Int // declares but doesn't initialize newIndex
-        
-        // Show a message
+    func nonRepeatingRandom(lastNumber: Int, maxValue: Int) -> Int {
+        var newIndex: Int
         repeat {
-            newIndex = Int.random(in: 0..<messages.count)
-        } while index == newIndex
-        
-        index = newIndex
-        messageLabel.text = messages[index]
-        
-        // Show an image
-        repeat {
-            newIndex = Int.random(in: 0..<numberOfImages)
-        } while imageIndex == newIndex
-        
-        imageIndex = newIndex
-        awesomeImageView.image = UIImage(named: "image\(imageIndex)")
-        
-        // Get a random number to use in our soundName file
-        repeat {
-            newIndex = Int.random(in: 0..<numberOfSounds)
-        } while soundIndex == newIndex
-        
-        soundIndex = newIndex
-        
-        // Play a sound
-        var soundName = "sound\(soundIndex)"
-        
+            newIndex = Int.random(in: 0..<maxValue)
+        } while lastNumber == newIndex
+        return newIndex
+    }
+    
+    func playSound(soundName: String) {
         // Can we load in the file soundName?
         if let sound = NSDataAsset(name: soundName) {
             // check is sound.data is a sound file
@@ -80,6 +49,34 @@ class ViewController: UIViewController {
             // if reading in the NSDataAsset didn't work, tell the developer / report the error.
             print("ERROR: file\(soundName) didn't load.")
         }
+    }
+    
+    @IBAction func showMessagePressed(_ sender: UIButton) {
+        let messages = ["You Are Fantastic!!!",
+                        "You Are Great!",
+                        "You Are Amazing!",
+                        "When the Genius Bar needs help, they call you",
+                        "You Brighten My Day!",
+                        "You are da bomb!",
+                        "Hey, fabulous!",
+                        "You are tremendous!",
+                        "You've got the design skills of Jony Ive!",
+                        "I can't wait to download your app!"]
+    
+        // Show a message
+        index = nonRepeatingRandom(lastNumber: index, maxValue: messages.count)
+        messageLabel.text = messages[index]
+        
+        // Show an image
+        imageIndex = nonRepeatingRandom(lastNumber: imageIndex, maxValue: numberOfImages)
+        awesomeImageView.image = UIImage(named: "image\(imageIndex)")
+        
+        // Get a random number to use in our soundName file
+        soundIndex = nonRepeatingRandom(lastNumber: soundIndex, maxValue: numberOfSounds)
+        
+        // Play a sound
+        let soundName = "sound\(soundIndex)"
+        playSound(soundName: soundName)
         
     }
     
